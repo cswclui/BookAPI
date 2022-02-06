dfb1b6312c172b79c570a6e7202b5a2d86ab1e76
Richard Lui <test@test.com>
2022-02-06 16:25:43 +0000

    Spring RESTful API (Task 1)

---
+++ src/main/java/com/example/book/Book.java
@@ -0,0 +1,43 @@
+package com.example.book;
+
+public class Book {
+    private int bookid;
+    private String title;
+    private String author;
+
+    public Book(int bookid, String title, String author) {
+        this.bookid = bookid;
+        this.title = title;
+        this.author = author;
+    }
+
+    public int getId() {
+        return bookid;
+    }
+
+    public void setId(int bookid) {
+        this.bookid = bookid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [author=" + author + ", bookid=" 
+                + bookid + ", title=" + title + "]";
+    }
+}
---
+++ src/main/java/com/example/book/BookController.java
@@ -0,0 +1,32 @@
+package com.example.book;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    private List<Book> books;
+
+    public BookController() {
+        System.out.println("BookController() called...");
+        books = new ArrayList<>();
+        books.add(new Book(1,"Hacking with Spring Boot 2.3","Greg L. Turnquist"));
+        books.add(new Book(2,"97 Things Every Java Programmer Should Know", "Kevlin Henney and Trisha Gee"));
+        books.add(new Book(3,"Spring Boot: Up and Running","Greg L. Turnquist "));
+    }
+
+    @GetMapping
+    public List<Book> list() {
+        return books;
+    }
+
+    @GetMapping("/{id}")
+    public Book get(@PathVariable int id) {
+        for (Book i: books){
+            if (i.getId()==id)
+                return i;
+        }
+        return null; //book not found
+    }
+}
