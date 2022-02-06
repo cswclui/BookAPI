6187c866b058bdcff5c8aa460b6ff178563d1036
Richard Lui <test@test.com>
2022-02-06 19:33:29 +0000

    Add Spring Tests

---
+++ src/test/java/com/example/book/MySpringBootTest.java
@@ -0,0 +1,38 @@
+package com.example.book;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class MySpringBootTest {
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void shouldReturnFirstBook() throws Exception {
+		Book b= this.restTemplate.getForObject("http://localhost:" + port + "/books/1", Book.class);
+		assertEquals("Hacking with Spring Boot 2.3", b.getTitle());
+		assertEquals("Greg L. Turnquist", b.getAuthor());
+	}
+
+	@Autowired
+	BookRepository repo;
+	
+	@Test
+	public void shouldReturnAllBook()  {
+		System.out.println("----shouldReturnBookListByHTTP-----");
+		System.out.println(repo.findAll());
+		System.out.println("---------");
+
+		//verify that there are 3 books
+		assertEquals(3, repo.findAll().size());
+	}
+}
---
+++ src/test/java/com/example/book/WebMVCTest.java
@@ -0,0 +1,42 @@
+package com.example.book;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.*;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;;
+
+@WebMvcTest
+@AutoConfigureMockMvc
+public class WebMVCTest{
+   @Autowired
+   private MockMvc mockMvc;
+   @MockBean
+   private BookService mockBookService;
+   @MockBean
+   private BookRepository mockBookRepository;
+
+   @Test
+   public void shouldReturnAllBooks() throws Exception 
+   {
+      when(mockBookService.list())
+      .thenReturn(List.of(
+         new Book(1,"Book1","Author1"),
+         new Book(1,"Book2","Author2")
+         ));
+
+      mockMvc.perform(get("/books"))
+         .andDo(print())
+         .andExpect(status().isOk()) //HTTP response code 200
+         .andExpect(jsonPath("$.size()").value(2))
+         .andExpect(jsonPath("$[0].title").value("Book1"))
+         .andExpect(jsonPath("$[0].author").value("Author1"))
+         .andExpect(jsonPath("$[1].title").value("Book2"))
+         .andExpect(jsonPath("$[1].author").value("Author2"));     
+   }  
+}
