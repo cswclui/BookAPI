1ef8589850d35f3f4bd457b1ac77d6d1d90c2525
Richard Lui <test@test.com>
2022-02-10 17:46:05 +0000

    Added command runner

--- src/main/java/com/example/book/BookApiApplication.java
+++ src/main/java/com/example/book/BookApiApplication.java
@@ -15,25 +15,4 @@ public class BookApiApplication {
 		SpringApplication.run(BookApiApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner demo(BookRepository repository) {
-		return (args) -> {
-			if (repository.findAll().isEmpty()){ //insert data if no data in the database
-				Book b1, b2, b3;
-				b1 = new Book("Hacking with Spring Boot 2.3","Greg L. Turnquist");
-				b2 = new Book("97 Things Every Java Programmer Should Know", "Kevlin Henney and Trisha Gee");
-				b3 = new Book("Spring Boot: Up and Running","Greg L. Turnquist ");
-				repository.save(b1);
-				repository.save(b2);
-				repository.save(b3);
-			}
-
-			// fetch all books
-			log.info("Books found with findAll():");
-			log.info("-------------------------------");
-			for (Book b : repository.findAll()) {
-				log.info(b.toString());
-			}
-		};
-	}
 }
