57fec068209e283cb810a25f19322b08b4d173a0
Richard Lui <test@test.com>
2022-02-10 17:58:26 +0000

    Update command runner

--- src/main/java/com/example/book/BookApiApplication.java
+++ src/main/java/com/example/book/BookApiApplication.java
@@ -2,15 +2,11 @@ package com.example.book;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookApiApplication {
-	private static final Logger log = LoggerFactory.getLogger(BookApiApplication.class);
-	
 	public static void main(String[] args) {
 		SpringApplication.run(BookApiApplication.class, args);
 	}
