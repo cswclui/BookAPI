bf1cd5955251f4ed3774b785e7195647202b2153
Richard Lui <test@test.com>
2022-02-14 09:52:09 +0000

    Update

--- src/test/java/com/example/book/MySpringBootTest.java
+++ src/test/java/com/example/book/MySpringBootTest.java
@@ -1,8 +1,6 @@
 package com.example.book;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
