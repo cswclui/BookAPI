9f9d0c36e33a36760745238f21da9edee6fd6942
Richard Lui <test@test.com>
2022-02-27 16:00:42 +0000

    update

---
+++ monitoring/gen.sh
@@ -0,0 +1,15 @@
+curl -s "\n" http://localhost:8080/books 
+curl -s "\n" http://localhost:8080/books/1 
+curl -s "\n" http://localhost:8080/books/2
+curl -s "\n" http://localhost:8080/books/3
+curl -s "\n" http://localhost:8080/books/4
+
+curl -X POST http://localhost:8080/books \
+-H 'content-type: application/json' \
+-d '{"title": "Spring in Action","author": "Craig Walls"}'
+
+ curl -X PUT http://localhost:8080/books \
+ -H 'content-type: application/json' \
+ -d '{"id": 4,"title": "Spring in Acton 2nd Ed","author": "Craig Walls"}'
+
+ curl -X DELETE http://localhost:8080/books/1
--- src/main/java/com/example/book/BookApiApplication.java
+++ src/main/java/com/example/book/BookApiApplication.java
@@ -1,10 +1,19 @@
 package com.example.book;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 public class BookApiApplication {
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+	  return registry -> registry.config().commonTags("application", "BOOKAPI");
+	}	
+		
 	public static void main(String[] args) {
 		SpringApplication.run(BookApiApplication.class, args);
 	}
